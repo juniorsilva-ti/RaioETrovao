c463fe10c2636e83dfb1eda99b23960e4ef3978c
juniorsilva-ti <162759693+juniorsilva-ti@users.noreply.github.com>
2024-06-03 00:41:43 -0300

    finalizado

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿﻿Console.WriteLine("--- Raio e Trovão ---\n");
+
+Console.WriteLine($"trovão a {3} segundos ==> raio caiu a {DistanciaDoRaioKm(3):N5} quilômetros");
+Console.WriteLine($"trovão a {6} segundos ==> raio caiu a {DistanciaDoRaioKm(6):N5} quilômetros");
+
+double DistanciaDoRaioKm(double segundos)
+{
+    const double VelocidadeDoSomKmS = 340.29 / 1000;
+    return segundos * VelocidadeDoSomKmS;
+}
+
+// Júnior Silva 
